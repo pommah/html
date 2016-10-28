367e0aeca4e846758451f4eba489d9223f5e9b2b
vladislav <vlad.kazmirchuk@gmail.com>
2016-10-29 00:54:15 +0300

    Изменение инфографики

--- application/controllers/controller_infographics.php
+++ application/controllers/controller_infographics.php
@@ -4,6 +4,7 @@ Class Controller_Infographics extends Authorized_Controller {
     public function __construct()
     {
         parent::__construct();
+        $this->model = new Model_Infographics();
         if($this->auth!=UserTypes::MINISTRY) {
             header("Location: /");
         }
@@ -14,6 +15,7 @@ Class Controller_Infographics extends Authorized_Controller {
 
     public function action_index()
     {
+        $this->data['statistic'] = $this->model->get_statistic_region();
         $this->view->generate('application/views/infographics/infographics_index.php','common.php',$this->data);
     }
 }
